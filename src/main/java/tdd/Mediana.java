196046cef10b1b064f77f8bcc99f017914f99c56
RenataGizzi <32368372+RenataGizzi@users.noreply.github.com>
2018-08-29 14:14:02 -0300

    mediana

---
+++ src/main/java/tdd/Mediana.java
@@ -0,0 +1,19 @@
+package tdd;
+
+import java.util.Arrays;
+
+public class Mediana {
+    int i;
+    double res = 0;
+    
+    public double calcularMediana(double vet[]) {
+            Arrays.sort(vet); //ordenar
+            int tipo = vet.length % 2;
+            if (tipo == 1) {
+                  return vet[((vet.length + 1) / 2) - 1];
+            } else {
+                  int m = vet.length / 2;
+                  return (vet[m - 1] + vet[m]) / 2;
+            }
+    }
+}
